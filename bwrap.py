a39d880516c3130fc0739e4f82006f63448f7d19
Fabio Valentini <decathorpe@gmail.com>
2017-07-12 19:48:32 +0200

    begin refactoring code

--- bwrap.py
+++ bwrap.py
@@ -18,7 +18,7 @@ class BubbleWrapper:
         self.symlinks = dict()
 
         self.unshares = list()
-        self.shares = list()
+        self.sharenet = False
 
         self.envs = dict()
         self.unenvs = list()
@@ -35,22 +35,93 @@ class BubbleWrapper:
         self.newtermsession = False
         self.diewithparent = False
 
+    def add_bind_mount(self, src: str, dest: str):
+        self.binds[dest] = src
+
+    def add_bind_mount_readonly(self, src: str, dest: str):
+        self.robinds[dest] = src
+
+    def add_bind_mount_dev(self, src: str, dest: str):
+        self.devbinds[dest] = src
+
+    def add_remount_readonly(self, dest: str):
+        self.remountros.append(dest)
+
+    def set_proc(self, dest: str):
+        self.proc = dest
+
+    def set_dev(self, dest: str):
+        self.dev = dest
+
+    def add_tmpfs(self, dest: str):
+        self.tmpfs.append(dest)
+
+    def add_mqueue(self, dest: str):
+        self.mqueues.append(dest)
+
+    def create_directory(self, dest: str):
+        self.dirs.append(dest)
+
+    def create_symlink(self, src: str, dest: str):
+        self.symlinks[dest] = src
+
+    def share_net(self):
+        self.sharenet = True
+
+    def add_unshare(self, share_type):
+        assert share_type in ["all", "user", "user-try", "ipc", "pid",
+                              "net", "uts", "cgroup", "cgroup-try"]
+        self.unshares.append(share_type)
+
+    def set_env(self, var: str, val: str):
+        self.envs[var] = val
+
+    def unset_env(self, var: str):
+        self.unenvs.append(var)
+
+    def set_uid(self, uid: int):
+        self.uid = str(uid)
+
+    def set_gid(self, gid: int):
+        self.gid = str(gid)
+
+    def set_chdir(self, chdir: str):
+        self.chdir = chdir
+
+    def set_hostname(self, hostname: str):
+        self.hostname = hostname
+
+    def set_exec_label(self, exec_label: str):
+        self.execlabel = exec_label
+
+    def set_file_label(self, file_label: str):
+        self.filelabel = file_label
+
+    def use_lock_file(self, lock_file: str):
+        self.lockfile = lock_file
+
+    def use_new_terminal_session(self):
+        self.newtermsession = True
+
+    def die_with_parent(self):
+        self.diewithparent = True
+
     def exec(self, comm: str, args: list) -> int:
         cmd = list()
 
         cmd.append("bwrap")
 
-        for bind in self.binds.keys():
+        for bind in self.binds:
             cmd.append("--bind")
             cmd.append(self.binds[bind])
             cmd.append(bind)
 
-        for robind in self.robinds.keys():
+        for robind in self.robinds:
             cmd.append("--ro-bind")
             cmd.append(self.robinds[robind])
             cmd.append(robind)
 
-        for devbind in self.devbinds.keys():
+        for devbind in self.devbinds:
             cmd.append("--dev-bind")
             cmd.append(self.devbinds[devbind])
             cmd.append(devbind)
@@ -59,11 +130,11 @@ class BubbleWrapper:
             cmd.append("--remount-ro")
             cmd.append(remountro)
 
-        if not (self.proc is None):
+        if not self.proc is not None:
             cmd.append("--proc")
             cmd.append(self.proc)
 
-        if not (self.dev is None):
+        if self.dev is not None:
             cmd.append("--dev")
             cmd.append(self.dev)
 
@@ -79,7 +150,7 @@ class BubbleWrapper:
             cmd.append("--dir")
             cmd.append(directory)
 
-        for symlink in self.symlinks.keys():
+        for symlink in self.symlinks:
             cmd.append("--symlink")
             cmd.append(self.symlinks[symlink])
             cmd.append(symlink)
@@ -87,10 +158,10 @@ class BubbleWrapper:
         for unshare in self.unshares:
             cmd.append("--unshare-" + unshare)
 
-        for share in self.shares:
-            cmd.append("--share-" + share)
+        if self.sharenet:
+            cmd.append("--share-net")
 
-        for env in self.envs.keys():
+        for env in self.envs:
             cmd.append("--setenv")
             cmd.append(env)
             cmd.append(self.envs[env])
@@ -99,21 +170,33 @@ class BubbleWrapper:
             cmd.append("--unsetenv")
             cmd.append(unenv)
 
-        if not (self.uid is None):
+        if self.uid is not None:
             cmd.append("--uid")
             cmd.append(self.uid)
 
-        if not (self.gid is None):
+        if self.gid is not None:
             cmd.append("--gid")
             cmd.append(self.gid)
 
-        if not (self.chdir is None):
+        if self.chdir is not None:
             cmd.append("--chdir")
             cmd.append(self.chdir)
 
-        if not (self.hostname is None):
+        if self.hostname is not None:
             cmd.append("--hostname")
             cmd.append(self.hostname)
+
+        if self.execlabel is not None:
+            cmd.append("--exec-label")
+            cmd.append(self.execlabel)
+
+        if self.filelabel is not None:
+            cmd.append("--file-label")
+            cmd.append(self.filelabel)
+
+        if self.lockfile is not None:
+            cmd.append("--lock-file")
+            cmd.append(self.lockfile)
 
         if self.newtermsession:
             cmd.append("--new-session")
@@ -137,7 +220,7 @@ class BubbleWrapper:
                 print("Process terminated with an error: {}".format(ret.returncode))
                 return ret.returncode
 
-        except IOError as e:
+        except IOError as error:
             print("Could not successfully execute subprocess. Original exception:")
-            print(e)
+            print(error)
             return -1
--- sbox.py
+++ sbox.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
 import os
-import subprocess
 import sys
 
 import bwrap
-
 
 HELP = """
 Usage:
@@ -17,122 +15,49 @@ if __name__ == "__main__":
 else:
     HELP = HELP.format("sbox")
 
-MOUNT_HOME = True
+MOUNT_HOME = False
 FRESH_XDG_RUNTIME_DIR = False
 
 
 def main() -> int:
-    cmd = list()
-
     if len(sys.argv) < 2:
         print(HELP)
         return 1
 
-    prog = sys.argv[1]
+    wrapper = bwrap.BubbleWrapper()
 
-    cmd.append("bwrap")
+    wrapper.add_bind_mount_readonly("/usr", "/usr")
 
-    # bind-mount /usr
-    cmd.append("--ro-bind")
-    cmd.append("/usr")
-    cmd.append("/usr")
+    wrapper.create_symlink("usr/bin", "/bin")
+    wrapper.create_symlink("usr/sbin", "/sbin")
+    wrapper.create_symlink("usr/lib64", "/lib64")
+    wrapper.create_symlink("usr/lib", "/lib")
 
-    # bind-mount $HOME
+    # wrapper.add_bind_mount_readonly("/etc/resolv.conf", "/etc/resolv.conf")
+    # wrapper.add_bind_mount_readonly("/etc/passwd", "/etc/passwd")
+    # wrapper.add_bind_mount_readonly("/etc/group", "/etc/group")
+    wrapper.add_bind_mount_readonly("/etc/fonts", "/etc/fonts")
+
     if MOUNT_HOME:
-        cmd.append("--bind")
-        cmd.append(os.getenv("HOME"))
-        cmd.append(os.getenv("HOME"))
+        wrapper.add_bind_mount(os.getenv("HOME"), os.getenv("HOME"))
+    else:
+        wrapper.create_directory(os.getenv("HOME"))
 
-    # symlink /usr/lib64 <- /lib64
-    cmd.append("--symlink")
-    cmd.append("usr/lib64")
-    cmd.append("/lib64")
+    wrapper.set_dev("/dev")
+    wrapper.set_proc("/proc")
+    wrapper.add_tmpfs("/tmp")
 
-    # symlink /usr/lib <- /lib
-    cmd.append("--symlink")
-    cmd.append("usr/lib")
-    cmd.append("/lib")
+    wrapper.add_unshare("all")
+    # wrapper.share_net()
 
-    # symlink /usr/bin <- /bin
-    cmd.append("--symlink")
-    cmd.append("usr/bin")
-    cmd.append("/bin")
-
-    # symlink /usr/sbin <- /sbin
-    cmd.append("--symlink")
-    cmd.append("usr/sbin")
-    cmd.append("/sbin")
-
-    # mount tmpfs at /tmp
-    cmd.append("--tmpfs")
-    cmd.append("/tmp")
-
-    # bind-mount resolv.conf from host
-    cmd.append("--ro-bind")
-    cmd.append("/etc/resolv.conf")
-    cmd.append("/etc/resolv.conf")
-
-    # mount proc filesystem
-    cmd.append("--proc")
-    cmd.append("/proc")
-
-    # mount dev filesystem
-    cmd.append("--dev")
-    cmd.append("/dev")
-
-    # unshare everything except network
-    cmd.append("--unshare-all")
-    cmd.append("--share-net")
-
-    # set XDG_RUNTIME_DIR
     if FRESH_XDG_RUNTIME_DIR:
         dir_xdg_runtime = "/run/user/" + str(os.getuid())
-
-        cmd.append("--dir")
-        cmd.append(dir_xdg_runtime)
-
-        cmd.append("--setenv")
-        cmd.append("XDG_RUNTIME_DIR")
-        cmd.append(dir_xdg_runtime)
-
+        wrapper.create_directory(dir_xdg_runtime)
+        wrapper.set_env("XDG_RUNTIME_DIR", dir_xdg_runtime)
     else:
-        cmd.append("--bind")
-        cmd.append(os.getenv("XDG_RUNTIME_DIR"))
-        cmd.append(os.getenv("XDG_RUNTIME_DIR"))
+        wrapper.add_bind_mount(os.getenv("XDG_RUNTIME_DIR"), os.getenv("XDG_RUNTIME_DIR"))
 
-    # bind-mount passwd file
-    cmd.append("--ro-bind")
-    cmd.append("/etc/passwd")
-    cmd.append("/etc/passwd")
-
-    # bind-mount group file
-    cmd.append("--ro-bind")
-    cmd.append("/etc/group")
-    cmd.append("/etc/group")
-
-    # bind-mount font configuration files
-    cmd.append("--ro-bind")
-    cmd.append("/etc/fonts")
-    cmd.append("/etc/fonts")
-
-    cmd.append(prog)
-
-    if len(sys.argv) > 2:
-        for arg in sys.argv[2:]:
-            cmd.append(arg)
-
-    try:
-        subprocess.run(cmd,
-                       stdin=sys.stdin,
-                       stderr=sys.stderr,
-                       stdout=sys.stdout,
-                       restore_signals=True)
-    except IOError:
-        return 1
-    except subprocess.SubprocessError:
-        return 1
-    finally:
-        return 0
+    return wrapper.exec(sys.argv[1], sys.argv[2:])
 
 
 if __name__ == "__main__":
